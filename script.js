e6e2b562ed8e420cf53d9752eef6f1e0db1eedbe
jasonejoseph <jasonejoseph@gmail.com>
2026-06-11 17:49:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const customers = [];
+
+const form = document.getElementById("customerForm");
+
+form.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const customer = {
+
+        id: Date.now(),
+
+        name: document.getElementById("fullname").value,
+
+        email: document.getElementById("email").value,
+
+        phone: document.getElementById("phone").value,
+
+        favoriteCategory:
+            document.getElementById("favorite").value
+    };
+
+    customers.push(customer);
+
+    const customerList = document.getElementById("customerList");
+
+const newCustomer = document.createElement("li");
+
+newCustomer.textContent =
+    customer.name + " - " + customer.email;
+
+customerList.appendChild(newCustomer);
+
+    console.clear();
+
+    console.log("Customer Array:");
+    console.table(customers);
+
+    alert("Customer successfully added!");
+
+    form.reset();
+
+
+});
